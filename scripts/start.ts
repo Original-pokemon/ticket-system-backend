318504feaf916676dce541cc04583ac62c5264f0
Original-pokemon <eremkin-02@mail.ru>
2023-09-20 16:40:23 +0300

    feat: database client

--- scripts/start.ts
+++ scripts/start.ts
@@ -1,12 +1,16 @@
 #!/usr/bin/env tsx
 
 import { config } from '#root/config.js';
-import { createServer } from '#root/index.js';
 import { logger } from '#root/logger.js';
+import DataBase from '#root/services/database/index.js';
+import { createServer } from '#root/services/server/index.js';
 import { onShutdown } from 'node-graceful-shutdown';
 
 try {
     const server = await createServer();
+    const database = new DataBase()
+
+    database.init()
 
     // Graceful shutdown
     onShutdown(async () => {
@@ -18,9 +22,6 @@ try {
         host: config.LISTEN_SERVER_HOST,
         port: config.LISTEN_SERVER_PORT,
     });
-
-    // if (config.isProd) {
-    // }
 
 } catch (error) {
     logger.error(error);
---
+++ src/services/database/index.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+
+class DataBase {
+  prisma?: PrismaClient
+
+  get client() {
+    if (this.prisma) {
+      return this.prisma
+    } else {
+      throw new Error('client is not initialized')
+    }
+  }
+
+  init() {
+    if (process.env.NODE_ENV === "production") {
+      this.prisma = new PrismaClient()
+    } else {
+      if (!this.prisma) {
+        this.prisma = new PrismaClient()
+      }
+    }
+    return true
+  }
+
+  async disconnect() {
+    if (this.prisma) {
+      try {
+        await this.prisma.$disconnect()
+        return true
+      } catch {
+        throw new Error('Error disconnecting client')
+      }
+    }
+  }
+}
+
+export default DataBase;
