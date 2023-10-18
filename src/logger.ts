a6f0d74f1f1cdeb1bd03214a65024983a66e1aaf
Original-pokemon <eremkin-02@mail.ru>
2023-10-18 09:28:26 +0300

    refactor:  create constants for targets

--- src/logger.ts
+++ src/logger.ts
@@ -3,32 +3,31 @@ import { fileURLToPath } from "node:url";
 import { config } from "#root/config.js";
 import { pino } from "pino";
 
+const developmentTargets = [
+  {
+    target: "pino-pretty",
+    level: config.LOG_LEVEL,
+    options: {
+      ignore: "pid,hostname",
+      colorize: true,
+      translateTime: true,
+    },
+  },
+];
+const productionTargets = [
+  {
+    target: "pino/file",
+    level: config.LOG_LEVEL,
+    options: {
+      destination: fileURLToPath(new URL("app.log", import.meta.url)),
+    },
+  },
+];
+
 export const logger = pino({
   level: config.LOG_LEVEL,
   transport: {
-    targets: [
-      ...(config.isDev
-        ? [
-          {
-            target: "pino-pretty",
-            level: config.LOG_LEVEL,
-            options: {
-              ignore: "pid,hostname",
-              colorize: true,
-              translateTime: true,
-            },
-          },
-        ]
-        : [
-          {
-            target: "pino/file",
-            level: config.LOG_LEVEL,
-            options: {
-              destination: fileURLToPath(new URL("app.log", import.meta.url)),
-            },
-          },
-        ]),
-    ],
+    targets: [...(config.isDev ? developmentTargets : productionTargets)],
   },
 });
 
